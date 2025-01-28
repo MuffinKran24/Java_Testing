a53d6a7a91216e6630eeea17888b8163538a3d17
Muffin <finn.brandt@gmx.de>
2025-01-28 21:49:14 +0100

    first commit

---
+++ DrawSquare.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class DrawSquare extends JPanel {
+
+    // @Override
+    // protected void paintComponent(Graphics g) {
+    //     super.paintComponent(g);
+    //     Graphics2D g2d = (Graphics2D) g;
+    //     g2d.drawRect(50, 50, 100, 100);
+    // }
+// 
+    // public static void main(String[] args) {
+    //     JFrame frame = new JFrame();
+    //     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //     frame.add(new DrawSquare());
+    //     frame.setSize(400, 200);
+    //     frame.setVisible(true);
+    // }
+}
---
+++ Graphics2D.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+import java.lang.Object;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+public abstract class Graphics2D
+extends Graphics;
+
